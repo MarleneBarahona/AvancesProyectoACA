8a21d5c7a0168d9fac0b51315c3446acc1001d73
MarleneBarahona <00118616@uca.edu.sv>
2020-06-03 18:16:54 -0600

    Primer commit. Cascaron html, solo se muestra un mapa de El Salvador.

---
+++ funcion.js
@@ -0,0 +1,10 @@
+var mymap = L.map('mapid').setView([13.8529939, -88.9053364], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 50,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+}).addTo(mymap);
